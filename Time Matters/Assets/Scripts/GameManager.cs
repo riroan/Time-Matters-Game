f3ce0afe2e2cac5ca478f92453b8ee082d00a7e0
김명기 <33053284+riroan@users.noreply.github.com>
2021-01-03 21:59:03 +0900

    setting

---
+++ Time Matters/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int currnet_ix = 0; // 현재 대사 인덱스
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // 저장한 데이터 불러오는 함수
+    void load_data()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
