666a4b1069e6728ca38882edd9d8326abdc8a4e1
김명기 <33053284+riroan@users.noreply.github.com>
2021-01-10 15:46:06 +0900

    typing

--- Time Matters/Assets/Scripts/ScriptController.cs
+++ Time Matters/Assets/Scripts/ScriptController.cs
@@ -4,32 +4,71 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class Dialog
+{
+    [TextArea]
+    public string line;         // 현재 대사
+    public Sprite cg;           // 지금 대사에 그림이 있는지
+    public bool isChoose;       // 지금 대사가 선택해야하는 대사인지
+}
+
 public class ScriptController : MonoBehaviour
 {
     int currentScriptIx = 0;
+    Dialog[] scripts;
+    [SerializeField] Text story;
+    [SerializeField] Image dialogImage;
+
     string[] script;
-    Text story;
-    
+    public bool isDialog = false;
+
     private void Start()
     {
         // script load
-        story = GameObject.Find("Story").GetComponent<Text>();
         script = File.ReadAllLines("Assets/Story/prologue.txt");
-        Debug.Log(script.Length);
+        scripts = new Dialog[script.Length];
+        //showDialog();
     }
-    private void Update()
+
+    void nextDialog()
     {
+        if (script.Length > currentScriptIx)
+        {
+            //story.text = script[currentScriptIx++];
+            StartCoroutine(Typing(story, script[currentScriptIx], 0.1f));
+        }
+    }
+
+    public void showDialog()
+    {
+        dialogImage.gameObject.SetActive(true);
     }
 
 
     private void OnMouseUp()
     {
-        if (gameObject.name == "board")
+        if (gameObject.name == "board" && !isDialog)
         {
-            if (script.Length > currentScriptIx)
-            {
-                story.text = script[currentScriptIx++];
-            }
+            nextDialog();
         }
+        else if (isDialog)
+        {
+            StopAllCoroutines();
+            story.text = script[currentScriptIx++];
+            isDialog = false;
+        }
+    }
+
+    IEnumerator Typing(Text txt, string message, float speed)
+    {
+        isDialog = true;
+        for (int i = 0; i < message.Length; i++)
+        {
+            txt.text = message.Substring(0, i + 1);
+            yield return new WaitForSeconds(speed);
+        }
+        isDialog = false;
+        currentScriptIx++;
     }
 }
