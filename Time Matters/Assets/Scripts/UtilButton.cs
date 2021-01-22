916921ec5936dfea2f8c1b0c15cd5c8e6987d49d
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2021-01-22 18:58:37 +0900

    ui_btn_design_and working
    designed complete
    however btn core need to be fix

--- Time Matters/Assets/Scripts/UtilButton.cs
+++ Time Matters/Assets/Scripts/UtilButton.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class UtilButton : MonoBehaviour
 { 
-    [SerializeField] GameObject options; // 매뉴 누르면 서브 메뉴 칸 나타난다.
-    [SerializeField] GameObject coninues; // 버튼 누르면 다시 원래대로
-    [SerializeField] GameObject settings; // 세팅 창으로 간다
-    [SerializeField] GameObject mainmenu; // 원래 본 화면으로 간다.
-    [SerializeField] GameObject insettings; // 인세팅 메뉴로 이동한다. 그곳에서 설정 가능하게 한다
-    [SerializeField] GameObject mainpage; // 맨 처음 메인 화면으로 되돌아간다.
+    [SerializeField] GameObject Bttn; // 매뉴 누르면 서브 메뉴 칸 나타난다.
+    [SerializeField] GameObject Bttn_comp; // 매뉴 누르면 서브 메뉴 칸 나타난다.
+    [SerializeField] GameObject Button_continue; // 버튼 누르면 다시 원래대로
+   // [SerializeField] GameObject Button_setting; // 세팅 창으로 간다
+   // [SerializeField] GameObject Button_exit; // 원래 본 화면으로 간다.
+   // [SerializeField] GameObject insettings; // 인세팅 메뉴로 이동한다. 그곳에서 설정 가능하게 한다
+   // [SerializeField] GameObject mainpage; // 맨 처음 메인 화면으로 되돌아간다.
 
 
     public void subButtonClick() // 왼쪽 위에 있는 메뉴 클릭 함수
     {
-        if (options.gameObject.activeSelf)
-            options.gameObject.SetActive(false);
-        else
-            options.gameObject.SetActive(true);
+        Bttn_comp.SetActive(true);
     }
 
-    public void continue_btn_Clicked() // 계속하기 버튼을 누를 때의 함수
+    public void Btutton_continue_Clicked() // 계속하기 버튼을 누를 때의 함수
     {
-        options.gameObject.SetActive(false);
+        Bttn_comp.SetActive(false);
 
     }
 
